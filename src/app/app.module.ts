eed78bd6947d5a44abe39e5f6928c7798d117f96
Annge001 <anngela0222@gmail.com>
2022-09-26 16:48:58 -0300

    cree component interpolaciones , se crea una interpolacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ZonaNorteComponent } from './components/zona-norte/zona-norte.component';
 import { ZonaCentroComponent } from './components/zona-centro/zona-centro.component';
 import { ZonaSurComponent } from './components/zona-sur/zona-sur.component';
+import { InterpolacionesComponent } from './components/interpolaciones/interpolaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ZonaNorteComponent,
     ZonaCentroComponent,
-    ZonaSurComponent
+    ZonaSurComponent,
+    InterpolacionesComponent
   ],
   imports: [
     BrowserModule
