c8ac32a3b780a9cc5d7d481e8ba2ebdde51f1093
Annge001 <106709390+Annge001@users.noreply.github.com>
2022-10-04 00:45:48 -0300

    Update formulario.component.ts

--- src/app/components/ejComunicacionComponentes/formulario/formulario.component.ts
+++ src/app/components/ejComunicacionComponentes/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormArray, FormBuilder, FormControl, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  formulario = this.formBuilder.group({
+    nombre: ['', [Validators.required]],
+    apellido: ['', [Validators.required]],
+    edad: ['', [Validators.required, Validators.min(21), this.validarEdad()]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    habilidades: new FormArray([new FormControl()])
+  })
+  private addUsuario: any;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
+  submitForm(): void {
+    console.log(this.formulario.value);
+    this.addUsuario.emit(this.formulario.value);
+  }
+
+  get habilidades(): FormArray {
+    return this.formulario.get('habilidades') as FormArray;
+  }
+
+  agregarHabilidad(): void {
+    this.habilidades.push(new FormControl());
+  }
+
+  validarEdad(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      return (!Number.isInteger(parseInt(control.value))) ? { errorEdad: true } : null;
+    }
+  }
 }
+
+
